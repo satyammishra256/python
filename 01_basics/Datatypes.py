0d62f5806fa58016271bdfdc36a6bbd8e19bcbfb
satyam mishra <paradisegangsta171@gmail.com>
2026-06-29 14:35:26 +0530

    Create Datatypes.py

---
+++ 01_basics/Datatypes.py
@@ -0,0 +1,31 @@
+#int
+print(7)
+
+#float
+print(90.5)
+
+#bool
+print(True)
+print(False)
+
+#str
+print("Satyam Mishra")
+
+#complex
+print(5+6j)
+
+#list
+print([1,2,3,4,5,6])
+
+#tuple
+print((1,2,3,4,5,6))
+
+#set
+print({1,2,3,4,5,6})
+
+#dict
+print({"name":"Satyam Mishra","gender":"Male"})
+
+# type() function
+print(type(7))
+print(type([1,2,3,4]))
