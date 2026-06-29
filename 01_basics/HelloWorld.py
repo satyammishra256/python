74143f4c5c59a1358aa2b3205bee6b7b8d76594e
satyam mishra <paradisegangsta171@gmail.com>
2026-06-29 14:09:02 +0530

    Create HelloWorld.py

---
+++ 01_basics/HelloWorld.py
@@ -0,0 +1,14 @@
+# PYTHON IS A CASE SENSITIVE LANGUAGE
+print('Hello World')
+print('hello',1,4.5,True)
+
+#sep
+print('hello',1,2.3,True)
+print('hello',1,2.3,True,sep='/')
+
+#end
+print("hello")
+print("world")
+
+print("hello",end="-")
+print("world")
