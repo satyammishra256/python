adc5e39837dd54f250670ea5375fda6f10ef8eeb
satyam mishra <paradisegangsta171@gmail.com>
2026-06-29 15:30:29 +0530

    Create variables.py

---
+++ 01_basics/variables.py
@@ -0,0 +1,41 @@
+#Variable => container to store data
+name="satyam"
+print(name)
+
+a=5
+b=6
+print(a+b)
+
+#Dynamic typing : isme variable ka datatype dene ki jaroorat nhi hoti
+a=5      #python supports dynamic typing
+print(a)
+#Static typing : isme variable ka datatype bhi dena hota hai
+#int a=5  we cant write this in python no need to write python automatcially understands the datatype
+
+#Dynamic binding => isme variable ek se jyada datatype ki value store kar sakta hai in the same program 
+a=6
+print(a)
+a="mishra"
+print(a)
+#Static binding => isme jo datatype declare kiya gya hai sabse pehle usi ki values store kar sakta
+
+#Methods to declare multiple variables
+a=1
+b=2
+c=3
+print(a,b,c)
+
+# method to declare them simultaneously
+a,b,c=6,7,8
+print(a,b,c)
+
+#method to declare if they have same values
+x=y=z=5
+print(x+y+z)
+
+# keywords => reserved words and we cannot use them as variables name
+# identifiers => names we give to any type of data in our program
+
+
+
+
